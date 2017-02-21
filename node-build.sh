c23f44e64a9eee256972ebbcba9ba6c5eeb44804
Jordan Mavrogeorge <jordomav@gmail.com>
2017-02-21 15:29:39 -0800

    basic node install script with homebrew

---
+++ node-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install node
+
+npm install -g nodemon
+npm install -g nvm
+npm install -g gulp
